5af48f108a2c44094ecfad3c8008012d7c28fb7f
Grzegorz Madeja <grzesio258@gmail.com>
2017-06-04 17:32:21 +0200

    Gui changes

--- FoodCalculatorGui/AddFoodWindow.xaml.cs
+++ FoodCalculatorGui/AddFoodWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace FoodCalculatorGui
 {
@@ -10,6 +11,66 @@ namespace FoodCalculatorGui
         public AddFoodWindow()
         {
             InitializeComponent();
+        }
+
+        private void nameTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (nameTextBox.Text == "Name")
+                nameTextBox.Text = string.Empty;
+        }
+
+        private void nameTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (nameTextBox.Text == string.Empty)
+                nameTextBox.Text = "Name";
+        }
+
+        private void weightTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (weightTextBox.Text == "Weight(g)")
+                weightTextBox.Text = string.Empty;
+        }
+
+        private void weightTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (weightTextBox.Text == string.Empty)
+                weightTextBox.Text = "Weight(g)";
+        }
+
+        private void caloriesTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (caloriesTextBox.Text == "Calories")
+                caloriesTextBox.Text = string.Empty;
+        }
+
+        private void caloriesTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (caloriesTextBox.Text == string.Empty)
+                caloriesTextBox.Text = "Calories";
+        }
+
+        private void carbohydratesTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void carbohydratesTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void fatTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void fatTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void proteinsTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void proteinsTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
